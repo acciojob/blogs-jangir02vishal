a9e3e6433476ac346ffdc56919e8dea6889f0f4e
Vishal Jangir <136950731+jangir02vishal@users.noreply.github.com>
2024-02-24 20:33:01 +0530

    Update UserService.java

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -5,24 +5,29 @@ import com.driver.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService {
+
     @Autowired
     UserRepository userRepository3;
 
-    public User createUser(String username, String password){
-
-
+    public User createUser(String username, String password) {
+        User user = new User(username, password);
+        return userRepository3.save(user);
     }
 
-    public void deleteUser(int userId){
-
+    public void deleteUser(int userId) {
+        userRepository3.deleteById(userId);
     }
 
-    public User updateUser(Integer id, String password){
-
+    public User updateUser(Integer id, String password) {
+        User user = userRepository3.findById(id).orElse(null);
+        if (user != null) {
+            user.setPassword(password);
+            return userRepository3.save(user);
+        }
+        return null;
     }
 }
