b700bbac0055ba6a434f1868d7de344c85d6c34e
Vishal Jangir <136950731+jangir02vishal@users.noreply.github.com>
2024-02-24 20:32:39 +0530

    Update ImageService.java

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -12,20 +12,35 @@ public class ImageService {
 
     @Autowired
     BlogRepository blogRepository2;
+
     @Autowired
     ImageRepository imageRepository2;
 
-    public Image addImage(Integer blogId, String description, String dimensions){
-        //add an image to the blog
-
+    public Image addImage(Integer blogId, String description, String dimensions) {
+        Blog blog = blogRepository2.findById(blogId).orElse(null);
+        if (blog != null) {
+            Image image = new Image(description, dimensions, blog);
+            return imageRepository2.save(image);
+        }
+        return null;
     }
 
-    public void deleteImage(Integer id){
-
+    public void deleteImage(Integer id) {
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
-        //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
-
+        Blog blog = blogRepository2.findById(id).orElse(null);
+        if (blog != null) {
+            List<Image> images = blog.getImages();
+            int count = 0;
+            for (Image image : images) {
+                if (image.getDimensions().equals(screenDimensions)) {
+                    count++;
+                }
+            }
+            return count;
+        }
+        return 0;
     }
 }
